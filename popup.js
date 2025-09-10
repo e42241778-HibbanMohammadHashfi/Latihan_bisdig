18447160a4fe3ffb0acdfe52a486536ec41550c6
e42241778-HibbanMohammadHashfi <e42241778@studen.polije.ac.id>
2025-09-10 14:59:44 +0700

    hallo word

---
+++ popup.js
@@ -0,0 +1,10 @@
+alert("halo");
+
+alert("hahahahah"), confirm(""), prompt("kamu manis deh")
+Date("6-04-2025") 
+parseInt("123") 
+setTimeout(functionName, 5000)
+
+function foo() {
+alert("Our function just ran!");
+} foo(); a
